6a2419425f62790a57c55e38af1e9e2ddbd1c016
Oscar Spencer <oscar@grain-lang.org>
2021-09-23 11:59:15 -0400

    json

--- templates/assemblyscript/src/custom.ts
+++ templates/assemblyscript/src/custom.ts
@@ -4,6 +4,8 @@ import { run } from "./lib"
 // EDIT custom_run TO CHANGE HOW DATA IS TRANSFORMED BEFORE PASSING INTO THE USER'S FUNCTION
 // EDIT lib.ts TO CHANGE THE TEMPLATE AND FUNCTION SIGNATURE THAT YOUR USERS WILL SEE
 
-export function custom_run(input: JSON.Obj): ArrayBuffer {
-	return run(input)
+export function custom_run(input: ArrayBuffer): ArrayBuffer {
+	let inputString = String.UTF8.decode(input);
+	let jsonObj: JSON.Obj = <JSON.Obj>(JSON.parse(inputString));
+	return run(jsonObj)
 }
