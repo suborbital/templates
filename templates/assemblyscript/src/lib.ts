17ab8be59d914a7afbdfbaacf8fefa3131604972
Oscar Spencer <oscar@grain-lang.org>
2021-09-23 12:05:32 -0400

    log payload

--- templates/assemblyscript/src/lib.ts
+++ templates/assemblyscript/src/lib.ts
@@ -2,9 +2,9 @@ import { JSON } from "assemblyscript-json";
 import { logInfo } from "@rocketshipinc/rocketship"
 
 export function run(input: JSON.Obj): ArrayBuffer {
-	let inStr = String.UTF8.decode(input)
-  
-	let out = "HEY THERE, " + inStr
+	let payload = input.toString()
+	
+	let out = "Shipment payload: " + payload
 
 	logInfo(out)
 
