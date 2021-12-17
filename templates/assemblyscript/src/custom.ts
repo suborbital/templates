d11e4ada0a1bc4144fa00e2df295db0b5492bee6
cohix <connor@suborbital.dev>
2021-12-17 08:19:01 -0500

    example of custom template

--- templates/assemblyscript/src/custom.ts
+++ templates/assemblyscript/src/custom.ts
@@ -1,8 +1,15 @@
-import { run } from "./lib"
+import { Post, Action, run } from "./lib"
+import {JSON} from "json-as"
 
 // EDIT custom_run TO CHANGE HOW DATA IS TRANSFORMED BEFORE PASSING INTO THE USER'S FUNCTION
 // EDIT lib.ts TO CHANGE THE TEMPLATE AND FUNCTION SIGNATURE THAT YOUR USERS WILL SEE
 
 export function custom_run(input: ArrayBuffer): ArrayBuffer {
-	return run(input)
+	const post = JSON.parse<Post>(String.UTF8.decode(input))
+
+	const action = run(post)
+
+	const output = JSON.stringify(action)
+
+	return output
 }
--- templates/assemblyscript/src/lib.ts
+++ templates/assemblyscript/src/lib.ts
@@ -1,11 +1,65 @@
 import { logInfo } from "@suborbital/suborbital"
 
-export function run(input: ArrayBuffer): ArrayBuffer {
-	let inStr = String.UTF8.decode(input)
-  
-	let out = "hey there, " + inStr
+const ACTION_BAN_USER: ActionType = {action: "ban"}
+const POST_STATUS_DELETED: PostStatus = {status: "deleted"}
 
-	logInfo(out)
+//@json
+export class Post {
+	post_id: string
+	title: string
+	body: string
+	created_at: string
+	upvotes: i32
+	downvote: i32
+	subreddit: Subreddit
+	author: Author
+}
 
-	return String.UTF8.encode(out)
+//@json
+export class Author {
+	user_id: string
+	name: string
+	karma: i32
+}
+
+//@json
+export class Subreddit {
+	subreddit_id: string
+	subreddit_name: string
+ }
+
+//@json
+export class Action {
+  action_type: ActionType
+  post_status: PostStatus
+}
+
+export class EmailAction {
+	action: Action
+	to: string
+	title: string
+	body: string
+}
+
+//@json
+export class ActionType {
+	action: string
+}
+
+//@json
+export class PostStatus {
+	status: string
+}
+
+export function run(post: Post): Action {  
+	let msg = "got post from, " + post.author.name
+
+	logInfo(msg)
+
+	const action: Action = {
+		action_type: ACTION_BAN_USER,
+		post_status: POST_STATUS_DELETED,
+	}
+
+	return action
 }
