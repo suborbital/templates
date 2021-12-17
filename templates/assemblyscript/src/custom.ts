fc91960ce25dbd7d9862404651bb2b4efe9243c7
cohix <connor@suborbital.dev>
2021-12-17 12:56:03 -0500

    add response object

--- templates/assemblyscript/src/custom.ts
+++ templates/assemblyscript/src/custom.ts
@@ -5,6 +5,12 @@ import {JSON} from "json-as"
 // EDIT custom_run TO CHANGE HOW DATA IS TRANSFORMED BEFORE PASSING INTO THE USER'S FUNCTION
 // EDIT lib.ts TO CHANGE THE TEMPLATE AND FUNCTION SIGNATURE THAT YOUR USERS WILL SEE
 
+//ts-ignore
+@json
+class ActionsResponse {
+	actions: Array<Action>
+}
+
 export function custom_run(input: ArrayBuffer): ArrayBuffer {
 	logInfo("got here 1")
 	logInfo(String.UTF8.decode(input))
@@ -14,8 +20,12 @@ export function custom_run(input: ArrayBuffer): ArrayBuffer {
 	
 	const actions = run(post)
 	logInfo("got here 3")
+
+	const response: ActionsResponse = {
+		actions: actions
+	}
 	
-	const output = JSON.stringify(actions)
+	const output = JSON.stringify(response)
 	logInfo("got here 4")
 
 	return String.UTF8.encode(output)
